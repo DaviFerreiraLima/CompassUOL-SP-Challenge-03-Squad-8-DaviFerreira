19b43c76f79426a097fb50d2afa06bb5e2faaa68
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-07-05 11:38:16 -0300

    Fixing some columns in the sql file

--- MySqlChallenge3Script.sql
+++ MySqlChallenge3Script.sql
@@ -1,50 +1,46 @@
+drop database challenge3
+CREATE DATABASE IF NOT EXISTS challenge3;
+USE challenge3;
 
-
-create database challenge3
-use challenge3
-
-CREATE TABLE category (
+CREATE TABLE IF NOT EXISTS category (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
   id INT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL,
-  roles VARCHAR(20) NOT NULL
+  password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE product (
+CREATE TABLE IF NOT EXISTS product (
   id INT PRIMARY KEY AUTO_INCREMENT,
   date DATE,
   description  VARCHAR(150) NOT NULL,
   name VARCHAR(255) NOT NULL,
   img_url VARCHAR(255),
-  price DECIMAL(10, 2),
-  category_id INT,
-  FOREIGN KEY (category_id) REFERENCES category(id)
+  price DECIMAL(10, 2)
 );
 
-CREATE TABLE role (
+CREATE TABLE IF NOT EXISTS role (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE product_category (
+CREATE TABLE IF NOT EXISTS product_category (
   product_id INT,
   category_id INT,
   PRIMARY KEY (product_id, category_id),
-  FOREIGN KEY (product_id) REFERENCES product(id),
-  FOREIGN KEY (category_id) REFERENCES category(id)
+  FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE,
+  FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE
 );
 
-CREATE TABLE user_role (
+CREATE TABLE IF NOT EXISTS user_role (
   user_id INT,
   role_id INT,
   PRIMARY KEY (user_id, role_id),
-  FOREIGN KEY (user_id) REFERENCES user(id),
-  FOREIGN KEY (role_id) REFERENCES role(id)
+  FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
 );
