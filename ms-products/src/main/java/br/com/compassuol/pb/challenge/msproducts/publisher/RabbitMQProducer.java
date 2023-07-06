40fad2513e563294d7a1a4e5354ebd088643d80a
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-07-06 11:23:03 -0300

    Feat: Adding the rabbitMQ configuration and publisher

---
+++ ms-products/src/main/java/br/com/compassuol/pb/challenge/msproducts/publisher/RabbitMQProducer.java
@@ -0,0 +1,32 @@
+package br.com.compassuol.pb.challenge.msproducts.publisher;
+
+import br.com.compassuol.pb.challenge.msproducts.payload.EmailDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+
+    @Value("${spring.rabbitmq.template.exchange}")
+    private String exchange = "email_exchange";
+
+    @Value("${spring.rabbitmq.template.routing-key}")
+    private String routingKey = "email_routing_key";
+
+    private final Logger LOGGER  = LoggerFactory.getLogger(RabbitMQProducer.class);
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public  void sendMessage(EmailDto emailDto) {
+        LOGGER.info(String.format("sent to ->%s",emailDto.toString()));
+        rabbitTemplate.convertAndSend(exchange,routingKey,emailDto);
+    }
+
+
+}
