f2972e8c6198d8f0302fa37a716f05436bd620e2
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-07-10 16:30:25 -0300

    Adding the user seeding for authorization in the producs ms

--- databases/UsersAndProductsDatabase.sql
+++ databases/UsersAndProductsDatabase.sql
@@ -1,4 +1,5 @@
-drop database challenge3
+DROP DATABASE IF EXISTS challenge3;
+
 CREATE DATABASE IF NOT EXISTS challenge3;
 USE challenge3;
 
@@ -18,7 +19,7 @@ CREATE TABLE IF NOT EXISTS user (
 CREATE TABLE IF NOT EXISTS product (
   id INT PRIMARY KEY AUTO_INCREMENT,
   date DATE,
-  description  VARCHAR(150) NOT NULL,
+  description VARCHAR(150) NOT NULL,
   name VARCHAR(255) NOT NULL,
   img_url VARCHAR(255),
   price DECIMAL(10, 2)
@@ -44,3 +45,15 @@ CREATE TABLE IF NOT EXISTS user_role (
   FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
   FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
 );
+
+INSERT INTO role (name) VALUES ('ROLE_OPERATOR');
+INSERT INTO role (name) VALUES ('ROLE_ADMIN');
+
+INSERT INTO user (first_name, last_name, email, password)
+VALUES ('Bob', 'Smith', 'bob@gmail.com', '$2a$10$Rt3N/pgtPy2D3A/nsCBYteKYKZfflqAaIwCy9FLksF2hcm1.1OFhG');
+
+SET @operatorId = (SELECT id FROM role WHERE name = 'ROLE_OPERATOR');
+SET @adminId = (SELECT id FROM role WHERE name = 'ROLE_ADMIN');
+
+INSERT INTO user_role (user_id, role_id) VALUES ((SELECT id FROM user WHERE email = 'bob@gmail.com'), @operatorId);
+INSERT INTO user_role (user_id, role_id) VALUES ((SELECT id FROM user WHERE email = 'bob@gmail.com'), @adminId);
