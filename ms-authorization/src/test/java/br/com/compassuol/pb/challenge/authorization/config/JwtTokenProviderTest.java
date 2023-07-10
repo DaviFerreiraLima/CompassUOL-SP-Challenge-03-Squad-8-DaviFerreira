27b94c4867857e74c1ee81b7f7415547564f6450
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-07-10 16:12:37 -0300

    Feat: Adding a JwtProvider test to the auth ms

---
+++ ms-authorization/src/test/java/br/com/compassuol/pb/challenge/authorization/config/JwtTokenProviderTest.java
@@ -0,0 +1,58 @@
+package br.com.compassuol.pb.challenge.authorization.config;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class JwtTokenProviderTest {
+
+    @InjectMocks
+    private JwtTokenProvider jwtTokenProvider;
+    @Test
+    public void validateTokenSuccess() {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                "bob@gmail.com",
+                null,
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+        String token = jwtTokenProvider.generateJwtToken(authentication);
+
+        String username = jwtTokenProvider.getUsername(token);
+
+        assertEquals("bob@gmail.com", username);
+    }
+
+
+    @Test
+    public void validateTokenInvalidToken() {
+        String token = "invalid";
+
+        assertThrows(Exception.class, () -> jwtTokenProvider.validateToken(token));
+    }
+
+
+
+    @Test
+    public void validateTokenInvalidTokenTampered() {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                "emma@gmail.com",
+                null,
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+        String token = jwtTokenProvider.generateJwtToken(authentication);
+
+
+        String tamperedToken = token + "X";
+
+        assertThrows(Exception.class, () -> jwtTokenProvider.validateToken(tamperedToken));
+    }
+}
